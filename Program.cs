3d064639ae15f059255cbab22e145fceb93fa962
leeool <lgonsalez02@gmail.com>
2022-04-08 22:20:28 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int idade;
+
+Console.Write("Digite sua idade: ");
+idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade < 18)
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("Você não pode doar sangue.");
+}
+
+
+else if (idade > 67)
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("Você não pode doar sangue.");
+}
+
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine("Você pode doar sangue!");
+}
+Console.ResetColor();
